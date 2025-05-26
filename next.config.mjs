0b75783d23d0ab1d23548b70cefcffdcc2c3880e
migacur <78935454+migacur@users.noreply.github.com>
2025-05-26 11:45:49 -0400

    Cambio array a objeto remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,69 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      'i.blogs.es',
-      'mundodeportivo.com',
-      'es.web.img3.acsta.net',
-      'phantom-marca-us.unidadeditorial.es',
-      'thecosmiccircus.com',
-      'imageio.forbes.com',
-      'www.infobae.com',
-      'res.cloudinary.com',
-      'live.staticflickr.com',
+      { protocol: "https", 
+        hostname: "i.blogs.es", 
+        port: "", 
+        pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "mundodeportivo.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "es.web.img3.acsta.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "phantom-marca-us.unidadeditorial.es",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "thecosmiccircus.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "imageio.forbes.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.infobae.com",
+        port: "",
+        pathname: "/**",
+      },
+       {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+        {
+        protocol: "https",
+        hostname: "live.staticflickr.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async headers() {
     return [
       {
         // Aplicar a todas las rutas de API
-        source: '/api/post/:id', // :path* captura todas las rutas bajo /api
+        source: "/api/post/:id", // :path* captura todas las rutas bajo /api
         headers: [
           {
-            key: 'x-user-data', // Nombre del header personalizado
-            value: '', // El valor se establecerá dinámicamente en el middleware
+            key: "x-user-data", // Nombre del header personalizado
+            value: "", // El valor se establecerá dinámicamente en el middleware
           },
         ],
       },
