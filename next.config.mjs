f12cfd78ee0b3c25badca893b5e63dd9685b5fd6
migacur <78935454+migacur@users.noreply.github.com>
2025-05-27 12:46:05 -0400

    nuevos cambios imagen

--- next.config.mjs
+++ next.config.mjs
@@ -43,6 +43,12 @@ const nextConfig = {
         pathname: "/**",
       },
        {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+       {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
