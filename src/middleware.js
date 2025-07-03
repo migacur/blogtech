2632b8205007be1a468e56abc25a13c4e5ac258d
migacur <78935454+migacur@users.noreply.github.com>
2025-07-03 00:01:59 -0400

    fix console.log mw

--- src/middleware.js
+++ src/middleware.js
@@ -42,7 +42,7 @@ export async function middleware(request) {
 
     // Manejar token expirado usando el código de error (jose usa códigos)
     if (error.code === 'ERR_JWT_EXPIRED') {
-       console("REDIRIGIENDO A INGRESAR...")
+       console.log("REDIRIGIENDO A INGRESAR...")
       const response = NextResponse.redirect(new URL("/ingresar", request.url));
       response.cookies.delete('myToken');
       return response;
@@ -57,7 +57,7 @@ export async function middleware(request) {
     }
     
     // Redirigir a login para rutas no-API
-    console("REDIRIGIENDO A INGRESAR...")
+    console.log("REDIRIGIENDO A INGRESAR...")
     return NextResponse.redirect(new URL("/ingresar", request.url));
   }
 }
